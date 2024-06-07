fdb52b6d5e13d94ba082da9b138af99f16eb58d9
nitishsingh41 <45527813+nitishsingh41@users.noreply.github.com>
2024-06-07 13:13:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,12 +22,13 @@ d = {
 }
 
 if user_input and button :
-    st.write(user_input)
-    test_sample = tokenizer([user_input], padding=True, truncation=True, max_length=512,return_tensors='pt')
+    with st.spinner('Predicting...'):
+        st.write(user_input)
+        test_sample = tokenizer([user_input], padding=True, truncation=True, max_length=512,return_tensors='pt')
     # test_sample
-    st.write('2')
-    output = model(**test_sample)
-    st.write("Logits: ",output.logits)
-    y_pred = np.argmax(output.logits.detach().numpy(),axis=1)
-    st.write("Prediction: ",d[y_pred[0]])
-    st.stop()
+        st.write('2')
+        output = model(**test_sample)
+        st.write("Logits: ",output.logits)
+        y_pred = np.argmax(output.logits.detach().numpy(),axis=1)
+        st.write("Prediction: ",d[y_pred[0]])
+        st.stop()
