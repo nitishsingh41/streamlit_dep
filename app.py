2fda16d27ebd5e7208059a8b13fe19c6b0db60e0
nitishsingh41 <45527813+nitishsingh41@users.noreply.github.com>
2024-06-08 13:36:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ if user_input and button :
     st.write(user_input)
     test_sample = tokenizer([user_input], padding=True, truncation=True, max_length=512,return_tensors='pt')
     test_sample = {k: v.half() for k, v in test_sample.items()}
+    test_sample=test_sample.long()
     # test_sample
     st.write('2')
     output = model(**test_sample)
