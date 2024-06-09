aada5bfdb410115aa370758f43ffde877b30a137
nitishsingh41 <45527813+nitishsingh41@users.noreply.github.com>
2024-06-09 11:33:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
 import streamlit as st
 import numpy as np
-
 import torch
-
-
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-tokenizer = AutoTokenizer.from_pretrained("AdamCodd/tinybert-sentiment-amazon")
-model = AutoModelForSequenceClassification.from_pretrained("AdamCodd/tinybert-sentiment-amazon")
-st.write('model loaded')
+st.header("Sentiment Analysis")
+with st.spinner('Loading model...'):
+    tokenizer = AutoTokenizer.from_pretrained("AdamCodd/tinybert-sentiment-amazon")
+    model = AutoModelForSequenceClassification.from_pretrained("AdamCodd/tinybert-sentiment-amazon")
 
-#tokenizer,model = get_model()
 
 user_input = st.text_area('Enter Text to Analyze')
 button = st.button("Predict")
 
 
-
-st.write('here')
 if user_input and button :
-
     inputs = tokenizer([user_input], padding=True, truncation=True, max_length=512,return_tensors='pt')
     with torch.no_grad():
         logits = model(**inputs).logits
 
     predicted_class_id = logits.argmax().item()
-    st.write(model.config.id2label[predicted_class_id])
+    predicted_class_label = model.config.id2label[predicted_class_id]
+    probabilities = torch.nn.functional.softmax(logits, dim=-1)[0].cpu().numpy()
+    score = probabilities[predicted_class_id]
+
+    # Display the results
+    st.write(f"Predicted Class: {predicted_class_label}")
+    st.write(f"Score: {score:.4f}")
